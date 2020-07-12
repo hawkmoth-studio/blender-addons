7aac5387baf8b6f125f6024c13cbd1dbadafb222
Vitaly Ogoltsov <vitaly.ogoltsov@me.com>
2020-07-12 21:58:04 +0300

    feat: Added install script for Windows.

---
+++ install.cmd
@@ -0,0 +1,26 @@
+ECHO OFF
+
+set BLENDER_VERSION=2.83
+set BLENDER_ADDONS_DIR=%APPDATA%\Blender^ Foundation\Blender\%BLENDER_VERSION%\scripts\addons
+
+
+CALL :addon_install hawkmoth_rigging_tools
+goto :eof
+
+
+:addon_install
+SETLOCAL
+set addon_name=%1
+set addon_source_dir=%addon_name%
+set addon_target_dir=%BLENDER_ADDONS_DIR%\%addon_name%
+
+echo Uninstalling '%addon_name%'...
+rmdir "%addon_target_dir%" /S /Q
+echo Installing '%addon_name%'...
+mkdir "%addon_target_dir%"
+robocopy "%addon_source_dir%" "%addon_target_dir%" /S /NP
+echo Addon '%addon_name%' successfully installed to '%addon_target_dir%'.
+ENDLOCAL & SET _result=0
+
+
+:eof
--- install.sh
+++ install.sh
@@ -4,17 +4,18 @@ BLENDER_VERSION="2.83"
 BLENDER_ADDONS_DIR="/Users/$(id -u -n)/Library/Application Support/Blender/${BLENDER_VERSION}/scripts/addons"
 
 
-function module_install() {
+function addon_install() {
   local addon_name="${1}"
-  local addon_dir="${BLENDER_ADDONS_DIR}/${addon_name}"
+  local addon_source_dir="./${addon_name}"
+  local addon_target_dir="${BLENDER_ADDONS_DIR}/${addon_name}"
 
-  echo "Uninstalling '${addon_dir}'..."
+  echo "Uninstalling '${addon_name}'..."
   __result=$(
     (
-      rm -rf "${addon_dir}"
+      rm -rf "${addon_target_dir}"
     ) 2>&1
   ) || (
-    echo "Uninstalling '${addon_dir}' from '${addon_dir}': failed!"
+    echo "Uninstalling '${addon_target_dir}' from '${addon_target_dir}': failed!"
     echo "${__result}"
     return 1
   ) || return 1
@@ -22,18 +23,18 @@ function module_install() {
   echo "Installing '${addon_name}'..."
   __result=$(
     (
-      mkdir -pv "${addon_dir}" &&
-      cp -rvf "./${addon_name}"/* "${addon_dir}/"
+      mkdir -pv "${addon_target_dir}" &&
+      cp -rvf "${addon_source_dir}"/* "${addon_target_dir}/"
     ) 2>&1
   ) || (
-    echo "Installing '${addon_name}' to '${addon_dir}': failed!"
+    echo "Installing '${addon_name}' to '${addon_target_dir}': failed!"
     echo "${__result}"
     return 1
   ) || return 1
 
-  echo "Module '${addon_name}' successfully installed to '${addon_dir}'."
+  echo "Addon '${addon_name}' successfully installed to '${addon_target_dir}'."
   return 0
 }
 
 
-module_install "hawkmoth_rigging_tools"
+addon_install "hawkmoth_rigging_tools"
